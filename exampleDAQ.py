9f81cf2bb4d988318997cf7f82987f9d634ad4d8
Alex Miles <almiles92@gmail.com>
2024-06-11 14:17:09 +0900

    Edited exampleDAQ.py

--- exampleDAQ.py
+++ exampleDAQ.py
@@ -15,4 +15,14 @@ ps = daq.runFullDAQ(outFile,
             0, 2, 1000,
             100, 2, 20000, 0, picoscopes[0])
 
+daq.seriesInitDaq(picoscopes[0])
+daq.seriesSetDaqSettings(
+            0, 2, 1100,
+            0, 2, 1000,
+            0, 2, 1000,
+            0, 2, 1000,
+            100, 2, 20000, 0)
+daq.seriesCollectData(outFile)
+daq.seriesCloseDaq()
+
 print("\nDAQ response:", ps)
