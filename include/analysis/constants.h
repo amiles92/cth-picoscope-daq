1832b7dacbb8dbfe9c9758888f8c31f9fc5c78d9
Nicolas.CHADEAU <nicolas.chadeau@uca.fr>
2024-07-30 20:04:19 +0900

    debugging analysis and verification with Yuki for MPPC bias voltages

--- include/analysis/constants.h
+++ include/analysis/constants.h
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <cstdio>
 #include <ctime>
+//#include <direct.h>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -22,13 +23,10 @@
 #include "TAttLine.h"
 #include "TCanvas.h"
 #include "TColor.h"
-#include "TEllipse.h"
 #include "TFile.h"
 #include "TGraph.h"
 #include "TH1.h"
 #include "TH2.h"
-#include "TH3.h"
-#include "THn.h"
 #include "THStack.h"
 #include "TLatex.h"
 #include "TLegend.h"
@@ -36,7 +34,6 @@
 #include "TLine.h"
 #include "TList.h"
 #include "TNamed.h"
-#include "TRandom3.h"
 #include "TStyle.h"
 #include "TSystem.h"
 #include "TText.h"
@@ -58,11 +55,37 @@ struct gaussParams{
 	double sigma;
 };
 
+struct plot2D{
+	TH2D* histo;
+	std::string name;
+};
+
+struct plot1D{
+	TH1D* histo;
+	std::string name;
+};
+
 const std::vector<int> ps6000VRanges{10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000};
 const bool showHeader(false);
 const bool doMovingAverage(false);
-const std::vector<std::string> MPPCsHV{"78V", "78.5V", "79V", "79.5V","80V", "80.5V", "81V", "81.5V", "82V", "82.5V", "83V"};
-const std::vector<std::string> LEDV{"Dark", "805mV", "810mV", "820mV", "830mV", "840mV", "850mV", "860mV", "870mV", "880mV", "890mV", "900mV", "905mV", "920mV"};
-const std::vector<std::string> picoscopeNames{"IW098-0028", "IW114-0004"};
+const std::vector<std::string> MPPCsHV{/*"78V", "78.5V", "79V", "79.5V","80V", "80.5V", */"81V"/*, "81.5V", "82V", "82.5V", "83V"*/}; 
+const std::vector<std::string> LEDV{"Dark", "805mV", "810mV", "820mV", "840mV", "870mV", "890mV"}; 
+const std::vector<std::string> PMTV{/*"1.2kV", "1.25kV", "1.3kV", "1.35kV", */"1.4kV"/*, "1.45kV", "1.5kV"*/}; 
+const std::vector<std::string> picoscopeNames{"IW098-0028", "IW114-0004"}; 
+
+// ROOT file branches variables
+TTree* tree;
+int channel=0;
+double pulseMinimum=0;
+int pulseMinimumSampleNumber=0;
+double pulseMinimumTime=0;
+double pulseMinimumMovingAverage=0;
+double integratedCharge=0;
+
+// Titles and naming should be modified if adding an histo
+const std::vector<std::vector<std::string>> titles{{"First 100 waveforms for ch", "waveforms"}, 
+                                                   {"Pulse time for ch", "pulseTime"}, 
+                                                   {"Pulse minimal value for ch", "pulseMinimum"}, 
+                                                   {"Integrated charge for ch", "integratedCharge"}}; 
 
 #endif //constants_h
