247d81bf81796d23a12976ef298a059ee0b50239
Alex Miles <almiles92@gmail.com>
2024-09-05 15:39:49 +1000

    Edited created directories to match launch_analysis

--- exec/install.sh
+++ exec/install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 cd ..
-if [ ! -d "plots/" ]; then
-	mkdir plots/
-fi
-if [ ! -d "plots/pre-analyse" ]; then
-	mkdir plots/pre-analyse
-fi
-if [ ! -d "plots/analyse" ]; then
-	mkdir plots/analyse
-fi
+# if [ ! -d "plots/" ]; then
+# 	mkdir plots/
+# fi
+# if [ ! -d "plots/analyse" ]; then
+# 	mkdir plots/analyse
+# fi
 if [ ! -d "pre-analyse" ]; then
 	mkdir pre-analyse
 fi
-if [ ! -d "pre-analyse/pre-analysed_root_files" ]; then
-	mkdir pre-analyse/pre-analysed_root_files
+if [ ! -d "pre-analyse/root-files" ]; then
+	mkdir pre-analyse/root-files
 fi
 if [ ! -d "pre-analyse/plots" ]; then
 	mkdir pre-analyse/plots
